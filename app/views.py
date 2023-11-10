6c3399c84cd8213e6fc9a2c293def79610aff936
Noor Elsheikh <noorcatch@gmail.com>
2023-11-10 17:20:34 +0000

    Email,name db entry form, updated requirements.txt

--- app/views.py
+++ app/views.py
@@ -4,12 +4,12 @@ from app.models import User
 from app.forms import EmailForm
 
 @app.route('/', methods=['GET', 'POST'])
-def add_email():
+def index():
     form = EmailForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User(email=form.email.data, firstname=form.firstname.data, lastname=form.lastname.data)
         db.session.add(user)
         db.session.commit()
         flash('Email added successfully!')
-        return redirect(url_for('add_email'))
-    return render_template('add_email.html', form=form)
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form)
--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app import app, db
 
-if __name__ == '__main__':
+with app.app_context():
     db.create_all()
+
+if __name__ == '__main__':
     app.run(debug=True)
