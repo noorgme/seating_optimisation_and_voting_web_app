c8c620e876be833a4ddbc5482a9e70a55679c5d1
EC2 Default User <ec2-user@ip-172-31-23-172.eu-west-1.compute.internal>
2023-11-14 14:57:51 +0000

    modify ignore

--- app/emailconfig.py
+++ app/emailconfig.py
@@ -1,6 +1,6 @@
-SMTP_SERVER = ''
+SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
-SMTP_USERNAME = ''
-SMTP_PASSWORD = ''
-FROM_EMAIL = ''
-FROM_NAME = ''
+SMTP_USERNAME = 'aiforgeassistant@gmail.com'
+SMTP_PASSWORD = 'yvwl uamt jpiv tovm'
+FROM_EMAIL = 'aiforgeassistant@gmail.com'
+FROM_NAME = 'Forge Team'
