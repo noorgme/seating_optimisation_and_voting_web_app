02d2f92f9445b87b39728442d2d3dfc8744b180f
Noor Elsheikh <105828590+noorgme@users.noreply.github.com>
2023-11-14 15:50:12 +0000

    Update emailconfig.py

--- app/emailconfig.py
+++ app/emailconfig.py
@@ -1,6 +1,6 @@
 cSMTP_SERVER = 'smtp.gmail.com'
 cPORT = 587
 cSMTP_USERNAME = 'aiforgeassistant@gmail.com'
-cSMTP_PASSWORD = 'yvwl uamt jpiv tovm'
+cSMTP_PASSWORD = ''
 cFROM_EMAIL = 'aiforgeassistant@gmail.com'
 cFROM_NAME = 'Forge Team'
